bbe7ee3ea0f2b6fb16d7192200b2635471be636a
Salhi Rayen <rayen.rsalhi9@gmail.com>
2026-01-16 23:10:34 +0100

    perf(event.actions): add caching to getSimilarEventsBySlug
    Use Next.js cache to improve performance of event fetching by reducing database calls

--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -1,8 +1,11 @@
 'use server'
+'use cache'
+import { cacheLife } from "next/cache"
 import { connectDB } from "../mongodb"
 import { Event } from "@/database"
 
 export const getSimilarEventsBySlug = async (slug: string) => {
+    cacheLife('hours')
     try {       
         await connectDB()
         const event = await Event.findOne({ slug })
